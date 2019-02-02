db79e0daa731d46c899db2d80b55e5a058f1f2e5
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2019-02-02 09:42:54 +0100

    Implemented cli interface.

--- src/PicturesToComicZipper.kt
+++ src/PicturesToComicZipper.kt
@@ -1,4 +1,6 @@
 import java.io.*
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.zip.ZipEntry
 
 
@@ -7,48 +9,72 @@ import java.util.zip.ZipOutputStream
 
 fun main(args: Array<String>) {
 
+    when {
+        args.isEmpty() -> println("Directory path can't be blank.")
+        else -> {
+            val location = args[0].trim()
 
-    File("/home/sanmi/Documents/test").listFiles().forEach {
+            when {
+                location.isBlank() -> {
+                    println("Please enter an existing directory path.")
+
+                }
+                else -> {
+                    val folderExists = Files.exists(Paths.get(location))
+
+                    when {
+                        folderExists -> {
+                            File(location).listFiles().forEach {
 
 
-        renameFiles(it)
+                                getFilesToZip(it, location)
 
+                            }
+                        }
+
+
+                    }
+                }
+
+            }
+            println("Files have been successfully renamed")
+        }
     }
 
 }
 
 
-fun renameFiles(file: File) {
+/***
+ * @Folder that contains pictures to be zipped.
+ * @location Location of the folder.
+ */
+fun getFilesToZip(folder: File, location: String) {
     when {
-        !file.isFile && file.isDirectory && !file.isHidden -> {
+        !folder.isFile && folder.isDirectory && !folder.isHidden -> {
 
             var filesToZip: MutableList<File> = mutableListOf<File>()
-            file.listFiles().forEach {
+            folder.listFiles().forEach {
                 try {
                     when {
                         it.isFile && !it.isDirectory -> {
 
-                            var filename: String = it.name
-
+                            val filename: String = it.name
 
                             if (filename.startsWith("RC", false)) {
                                 filesToZip.add(it)
-
                             }
-
 
                         }
                     }
-
 
                 } catch (e: StringIndexOutOfBoundsException) {
                     println("Files not renamed. Try again")
                 }
 
-                zipFiles(file.nameWithoutExtension, filesToZip)
-
             }
 
+            zipFiles(folder.nameWithoutExtension, filesToZip, location)
+            filesToZip = mutableListOf<File>()
 
         }
     }
@@ -56,49 +82,41 @@ fun renameFiles(file: File) {
 
 }
 
-fun zipFiles(zipName: String, list: MutableList<File>) {
-    val f = FileOutputStream("/home/sanmi/Documents/test/$zipName.cbr ")
+/***
+ * @zipname: The name of the zip to be created
+ *
+ * @list: The list of files to be zipped.
+ */
+fun zipFiles(zipName: String, list: MutableList<File>, location: String) {
 
+    val strippedZipName = zipName.substringBefore("-")
+    println(strippedZipName)
+    val f = FileOutputStream("$location/$strippedZipName.cbz")
     val out = ZipOutputStream(BufferedOutputStream(f))
-
 
     list.forEach {
 
-
-//        try {
-//            val fis = FileInputStream(it);
-//            val ze = ZipEntry(it.name);
-//            System.out.println("Zipping the file: " + it.getName());
-//            out.putNextEntry(ze);
-//
-//            System.out.println("Done... Zipped the files...");
-//
-//        } catch (e: IOException) {
-//
-//        }
-
-        val fileInputStream : FileInputStream = FileInputStream(it)
-        val zipEntry : ZipEntry = ZipEntry(it.name)
+        val fileInputStream: FileInputStream = FileInputStream(it)
+        val zipEntry: ZipEntry = ZipEntry(it.name)
         out.putNextEntry(zipEntry)
 
         val bytes = ByteArray(1024)
         var lenght = fileInputStream.read(bytes)
-        System.out.println("Zipping the file: " + it.getName());
-        while (lenght >= 0){
+
+        while (lenght >= 0) {
             out.write(bytes, 0, lenght)
             lenght = fileInputStream.read(bytes)
         }
-        System.out.println("Done... Zipped the files...");
-
 
     }
-
+    println("Done... Zipped the files... $strippedZipName ")
 
     out.closeEntry()
 
 
     out.close()
 
-
 }
 
+
+//Todo clean up folder leaving the zipped comic.
